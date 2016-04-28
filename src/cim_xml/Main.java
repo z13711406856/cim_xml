381cde5fe1f109b479e483dacb9662111fd458dd
z13711406856 <393905601@qq.com>
2016-04-29 02:14:18 +0800

    添加了日志，还有配置保存

--- src/cim_xml/Main.java
+++ src/cim_xml/Main.java
@@ -1,6 +1,8 @@
 package cim_xml;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
@@ -9,6 +11,7 @@ import java.sql.SQLException;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Map.Entry;
+import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -38,9 +41,53 @@ public class Main {
 	
 	public static final Logger log=LogManager.getLogger(Main.class);
 	   //123为密码
-	public static void main(String args[]) throws ClassNotFoundException{
+	public static void main(String args[]){
+		
 		Date begin=new Date();
+		try{
+			begin();
+		}catch(Exception e){
+			Main.log.error(e.getMessage(), e);
+		}
+		
+		/*String url1="file://F:/g各种PDF/java/dom4j-1.6.1dom4j-1.6.1/xml/web.xml";*/
+	/*	for(int i=0;i<0;i++){
+			System.out.println("1");
+		}
+		URL url=new URL("file:///F:/g各种PDF/java/dom4j-1.6.1/dom4j-1.6.1/xml/web.xml");
+		System.out.println(url);
+		CimXml cim=new CimXml(url);
+		cim.bar();
+		*/
+	/*	String s="Naming.name";
+		System.out.println(".");
+		String[] sList=s.split("\\.");
+		System.out.println(sList.length);
+		System.out.println(sList[0]);*/
+		/*main.createTableSql(cimXml);*/
 
+	/*	main.insertObject();*/
+		Date end=new Date();
+		Main.log.info("take "+(end.getTime()-begin.getTime())/1000+"s");
+		
+	}
+	//make create sql
+	/**
+	 * @throws ClassNotFoundException 
+	 * @throws IOException 
+	 * 
+	 */
+	private static void begin() throws ClassNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		File f=new File("resource.dat");
+		final Properties pro=new Properties();
+		if(f.exists()){
+			FileReader reader=new FileReader(f);
+			pro.load(reader);
+			reader.close();
+
+		}
+		
 		final CimXml cimXml=new CimXml();
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		
@@ -62,19 +109,25 @@ public class Main {
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
-				System.out.println("cimPath");
-				FileDialog filedlg=new FileDialog(shell,SWT.OPEN);
-				filedlg.setText("选择cim/xml文件");
-				filedlg.setFilterPath(System.getProperty("user.home"));
-				filedlg.setFilterExtensions(new String[]{"*.xml"});
-				filedlg.setFilterNames(new String[]{"Xml,*.xml"});
-				String filePath=filedlg.open();
-				if(filePath!=null){
-					cimPath.setText(filePath);
-					File cimFile=new File(filePath);
-					cimXml.setUrl(cimFile);
-					System.out.println(filePath);
+				try{
+					System.out.println("cimPath");
+					FileDialog filedlg=new FileDialog(shell,SWT.OPEN);
+					filedlg.setText("选择cim/xml文件");
+					filedlg.setFilterPath(System.getProperty("user.home"));
+					filedlg.setFilterExtensions(new String[]{"*.xml"});
+					filedlg.setFilterNames(new String[]{"Xml,*.xml"});
+					String filePath=filedlg.open();
+					if(filePath!=null){
+						cimPath.setText(filePath);
+						File cimFile=new File(filePath);
+					
+						pro.setProperty("cimPath", filePath);
+						System.out.println(filePath);
 				}
+				}catch(Exception e1){
+					Main.log.error(e1.getMessage(), e1);
+				}
+				
 			}
 		});
 		btnNewButton.setText("\u6D4F\u89C8");
@@ -90,15 +143,21 @@ public class Main {
 		button.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
-				System.out.println("sqlPath");
-				FileDialog filedlg=new FileDialog(shell,SWT.OPEN);
-				filedlg.setText("选择sql文件");
-				filedlg.setFilterPath(System.getProperty("user.home"));
-				filedlg.setFilterExtensions(new String[]{"*.sql"});
-				filedlg.setFilterNames(new String[]{"Sql,*.sql"});
-				String filePath=filedlg.open();
-				sqlPath.setText(filePath);
-				System.out.println(filePath);
+				try{
+					System.out.println("sqlPath");
+					FileDialog filedlg=new FileDialog(shell,SWT.OPEN);
+					filedlg.setText("选择sql文件");
+					filedlg.setFilterPath(System.getProperty("user.home"));
+					filedlg.setFilterExtensions(new String[]{"*.sql"});
+					filedlg.setFilterNames(new String[]{"Sql,*.sql"});
+					String filePath=filedlg.open();
+					sqlPath.setText(filePath);
+					pro.setProperty("sqlPath", filePath);
+					System.out.println(filePath);
+				}catch(Exception e1){
+					Main.log.error(e1.getMessage(), e1);
+				}
+
 			}
 		});
 		button.setText("\u6D4F\u89C8");
@@ -134,11 +193,14 @@ public class Main {
 		beginButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
+				try{
+				
 				FileWriter fWriter=null;
 				MessageBox box=new MessageBox(shell,SWT.ICON_INFORMATION|SWT.YES);
 				try{
 					fWriter=new FileWriter(sqlPath.getText(),false);
 					cimXml.setAnalyzeMode(AnalyzeMode.table);
+					cimXml.setUrl(new File(cimPath.getText()));
 					cimXml.buildTableSAX();
 					cimXml.createTableSql(fWriter);
 					fWriter.close();
@@ -157,7 +219,10 @@ public class Main {
 					box.setMessage("提醒");
 					box.setMessage(e1.getClass().getName());
 					box.open();
-					e1.printStackTrace();
+					throw e1;
+				}
+				}catch(Exception e2){
+					Main.log.error(e2.getMessage(), e2);
 				}
 			}
 		});
@@ -171,8 +236,10 @@ public class Main {
 				Connection conn=null;
 				MessageBox box=new MessageBox(shell,SWT.ICON_INFORMATION|SWT.YES);
 				try{
+				
 				conn = DriverManager.getConnection(connectString.getText(), account.getText(),password.getText());
 				cimXml.setAnalyzeMode(AnalyzeMode.insert);
+				cimXml.setUrl(new File(cimPath.getText()));
 				cimXml.setCon(conn);
 				cimXml.buildTableSAX();
 				Iterator<Entry<String, Integer>> it=cimXml.recordCount.entrySet().iterator();
@@ -186,7 +253,8 @@ public class Main {
 				}
 				cimXml.getDistinctTableMap().clear();
 				cimXml.getRecordCount().clear();
-				
+				pro.setProperty("connectString",connectString.getText());
+				pro.setProperty("account", account.getText());
 				box.setMessage("提醒");
 				box.setMessage(mess.toString());
 				box.open();
@@ -194,13 +262,9 @@ public class Main {
 					box.setMessage("提醒");
 					box.setMessage(e1.getClass().getName());
 					box.open();
-					e1.printStackTrace();
+					Main.log.error(e1.getMessage(), e1);
 				}
-				finally{
-					
-				}
-			}
-		});
+				}});
 		insert.setText("\u5F00\u59CB\u63D2\u5165");
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
@@ -217,6 +281,20 @@ public class Main {
 		lblSqlsql.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
 		lblSqlsql.setText("sql\u8F93\u51FA\u8DEF\u5F84:\u7528\u4E8E\u8BBE\u5B9A\u8F93\u51FA\u7684SQL\u6587\u4EF6\u8DEF\u5F84");
 		new Label(shell, SWT.NONE);
+		if(pro.getProperty("cimPath")!=null){
+			cimPath.setText(pro.getProperty("cimPath"));
+		}
+		if(pro.getProperty("sqlPath")!=null){
+			sqlPath.setText(pro.getProperty("sqlPath"));
+		}
+		if(pro.getProperty("connectString")!=null){
+			connectString.setText(pro.getProperty("connectString"));
+		}
+		if(pro.getProperty("account")!=null){
+			account.setText(pro.getProperty("account"));
+		}
+		
+
 		shell.pack();
 		shell.open();
 		while (!shell.isDisposed()) {
@@ -224,30 +302,16 @@ public class Main {
 				display.sleep();
 			}
 		}
-	
-	
-		/*String url1="file://F:/g各种PDF/java/dom4j-1.6.1dom4j-1.6.1/xml/web.xml";*/
-	/*	for(int i=0;i<0;i++){
-			System.out.println("1");
+		FileWriter writer;
+		try {
+			writer = new FileWriter(f,false);
+			pro.store(writer,"");
+			writer.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
-		URL url=new URL("file:///F:/g各种PDF/java/dom4j-1.6.1/dom4j-1.6.1/xml/web.xml");
-		System.out.println(url);
-		CimXml cim=new CimXml(url);
-		cim.bar();
-		*/
-	/*	String s="Naming.name";
-		System.out.println(".");
-		String[] sList=s.split("\\.");
-		System.out.println(sList.length);
-		System.out.println(sList[0]);*/
-		/*main.createTableSql(cimXml);*/
-
-	/*	main.insertObject();*/
-		Date end=new Date();
-		Main.log.info("take "+(end.getTime()-begin.getTime())/1000+"s");
-		
 	}
-	//make create sql
 	
 }
 class ConParam{
